2eaa82f939400626ba797d72b8646725855a7693
Josh <joas@spacesaver.com>
2021-05-03 10:27:12 -0500

    cleanup of views

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
-
 from rest_framework.views import APIView
-
 from rest_framework.response import Response
-
-#from django.http import Response
-
 from rest_framework.reverse import reverse
-
 from .models import Todo
-
 from .serializers import TodoSerializer
 
 
